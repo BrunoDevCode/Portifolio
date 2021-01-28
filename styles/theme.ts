edd9a1d56a5f299599d68160fe2cf0249427755e
Bruno Henrique dos Santos <brunohenriquesantos272@gmail.com>
2021-01-28 00:44:46 -0300

    Create a theme

---
+++ styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+const Theme: DefaultTheme = {
+  colors: {
+    text: '#60656F',
+    title: '#131112',
+    primary: '#279AF1',
+    secondBackground: '#F7F7FF',
+  }
+}
+
+export default Theme;
